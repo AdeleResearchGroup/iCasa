b5cf263b26ceadbcef6c70f14a0a81412d4f0ee2
aygalinc <colin.aygalinc@gmail.com>
2014-05-19 14:07:51 +0200

    Using fushia

---
+++ platform/physical-devices/philips/importers/src/main/java/fr/liglab/adele/icasa/philips/importers/PhilipsHueImporter.java
@@ -0,0 +1,155 @@
+/**
+ *
+ *   Copyright 2011-2013 Universite Joseph Fourier, LIG, ADELE Research
+ *   Group Licensed under a specific end user license agreement;
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://adeleresearchgroup.github.com/iCasa/snapshot/license.html
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package fr.liglab.adele.icasa.philips.importers;
+
+/*
+ * #%L
+ * OW2 Chameleon - Fuchsia Importer Philips Hue
+ * %%
+ * Copyright (C) 2009 - 2014 OW2 Chameleon
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.apache.felix.ipojo.Factory;
+import org.apache.felix.ipojo.annotations.*;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
+import org.ow2.chameleon.fuchsia.core.FuchsiaUtils;
+import org.ow2.chameleon.fuchsia.core.component.AbstractImporterComponent;
+import org.ow2.chameleon.fuchsia.core.declaration.ImportDeclaration;
+import org.ow2.chameleon.fuchsia.core.exceptions.BinderException;
+import fr.liglab.adele.icasa.philips.importers.utils.PhilipsHueImportDeclarationWrapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+@Component
+@Provides(specifications = {org.ow2.chameleon.fuchsia.core.component.ImporterService.class})
+public class PhilipsHueImporter extends AbstractImporterComponent {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PhilipsHueImporter.class);
+
+    private final BundleContext context;
+
+    private ServiceReference serviceReference;
+
+    @Requires(filter = "(factory.name=philipsHueLight)")
+    private Factory philipsHueLightFactory;
+
+    private Map<String,ServiceRegistration> lamps=new HashMap<String, ServiceRegistration>();
+    private Map<String,ServiceRegistration> bridges=new HashMap<String, ServiceRegistration>();
+
+    @ServiceProperty(name = "target", value = "(&(discovery.philips.device.name=*)(scope=generic))")
+    private String filter;
+
+    @ServiceProperty(name = Factory.INSTANCE_NAME_PROPERTY)
+    private String name;
+
+    public PhilipsHueImporter(BundleContext context) {
+        this.context = context;
+    }
+
+    @PostRegistration
+    public void registration(ServiceReference serviceReference) {
+        setServiceReference(serviceReference);
+    }
+
+    @Validate
+    public void validate() {
+        LOG.info("Philips hue Importer is up and running");
+    }
+
+    @Invalidate
+    public void invalidate() {
+
+        LOG.info("Cleaning up instances into Philips hue Importer");
+
+        cleanup();
+
+    }
+
+    private void cleanup(){
+
+        for(Map.Entry<String,ServiceRegistration> lampEntry:lamps.entrySet()){
+            lamps.remove(lampEntry.getKey()).unregister();
+        }
+
+        for(Map.Entry<String,ServiceRegistration> bridgeEntry:bridges.entrySet()){
+            bridges.remove(bridgeEntry.getKey()).unregister();
+        }
+
+    }
+
+    @Override
+    protected void useImportDeclaration(ImportDeclaration importDeclaration) throws BinderException {
+
+        LOG.info("philips hue importer triggered");
+
+        PhilipsHueImportDeclarationWrapper pojo= PhilipsHueImportDeclarationWrapper.create(importDeclaration);
+
+        try {
+
+            FuchsiaUtils.loadClass(context,pojo.getType());
+
+            Dictionary<String, Object> props = new Hashtable<String, Object>();
+
+            ServiceRegistration lampService=context.registerService(pojo.getType(),pojo.getObject(),props);
+
+            super.handleImportDeclaration(importDeclaration);
+
+            lamps.put(pojo.getId(),lampService);
+
+        } catch (ClassNotFoundException e) {
+            LOG.error("Failed to load type {}, importing process aborted.", pojo.getType(), e);
+        }
+
+
+    }
+
+    @Override
+    protected void denyImportDeclaration(ImportDeclaration importDeclaration) throws BinderException {
+
+        PhilipsHueImportDeclarationWrapper pojo= PhilipsHueImportDeclarationWrapper.create(importDeclaration);
+
+        try {
+            lamps.remove(pojo.getId()).unregister();
+        }catch(IllegalStateException e){
+            LOG.error("failed unregistering lamp", e);
+        }
+
+        unhandleImportDeclaration(importDeclaration);
+    }
+
+
+    public String getName() {
+        return name;
+    }
+}
+
---
+++ platform/physical-devices/philips/importers/src/main/java/fr/liglab/adele/icasa/philips/importers/utils/PhilipsHueImportDeclarationWrapper.java
@@ -0,0 +1,106 @@
+/**
+ *
+ *   Copyright 2011-2013 Universite Joseph Fourier, LIG, ADELE Research
+ *   Group Licensed under a specific end user license agreement;
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://adeleresearchgroup.github.com/iCasa/snapshot/license.html
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package fr.liglab.adele.icasa.philips.importers.utils;
+
+/*
+ * #%L
+ * OW2 Chameleon - Fuchsia Importer Philips Hue
+ * %%
+ * Copyright (C) 2009 - 2014 OW2 Chameleon
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.osgi.framework.Filter;
+import org.ow2.chameleon.fuchsia.core.FuchsiaUtils;
+import org.ow2.chameleon.fuchsia.core.declaration.ImportDeclaration;
+import org.ow2.chameleon.fuchsia.core.exceptions.BinderException;
+import org.ow2.chameleon.fuchsia.core.exceptions.InvalidFilterException;
+
+import java.util.Map;
+
+import static org.ow2.chameleon.fuchsia.core.declaration.Constants.ID;
+import static fr.liglab.adele.icasa.philips.importers.utils.Constants.*;
+
+public class PhilipsHueImportDeclarationWrapper {
+
+    private static Filter declarationFilter = buildFilter();
+
+    private String id;
+    private String name;
+    private String type;
+    private Object object;
+
+    private PhilipsHueImportDeclarationWrapper() {
+
+    }
+
+    private static Filter buildFilter() {
+        Filter filter;
+        String stringFilter = String.format("(&(%s=*)(%s=*)(%s=*)(%s=*))",
+                ID,
+                DISCOVERY_PHILIPS_DEVICE_NAME, DISCOVERY_PHILIPS_DEVICE_TYPE, DISCOVERY_PHILIPS_DEVICE_OBJECT);
+        try {
+            filter = FuchsiaUtils.getFilter(stringFilter);
+        } catch (InvalidFilterException e) {
+            throw new IllegalStateException(e);
+        }
+        return filter;
+    }
+
+    public static PhilipsHueImportDeclarationWrapper create(ImportDeclaration importDeclaration) throws BinderException {
+        Map<String, Object> metadata = importDeclaration.getMetadata();
+
+        if (!declarationFilter.matches(metadata)) {
+            throw new BinderException("Not enough information in the metadata to be used by the phillips hue importer");
+        }
+        PhilipsHueImportDeclarationWrapper wrapper = new PhilipsHueImportDeclarationWrapper();
+
+        wrapper.id = (String) metadata.get(ID);
+        wrapper.name = (String) metadata.get(DISCOVERY_PHILIPS_DEVICE_NAME);
+        wrapper.type = (String) metadata.get(DISCOVERY_PHILIPS_DEVICE_TYPE);
+        wrapper.object = metadata.get(DISCOVERY_PHILIPS_DEVICE_OBJECT);
+
+        return wrapper;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+}
