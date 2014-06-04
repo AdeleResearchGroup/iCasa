376fac49163e999df05f1ff707de9a37b5012313
aygalinc <colin.aygalinc@gmail.com>
2014-06-04 15:55:29 +0200

    Add Zigbee Dongle Factory

---
+++ platform/physical-devices/zigbee-fushia/zigbee-dongle-factory/src/main/java/fr/liglab/adele/icasa/zigbee/dongle/factory/ZigbeeDongleFactory.java
@@ -0,0 +1,25 @@
+package fr.liglab.adele.icasa.zigbee.dongle.factory;
+
+import org.apache.felix.ipojo.*;
+import org.apache.felix.ipojo.annotations.*;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
+import org.ow2.chameleon.fuchsia.core.component.AbstractImporterComponent;
+import org.ow2.chameleon.fuchsia.core.declaration.ImportDeclaration;
+import org.ow2.chameleon.fuchsia.core.exceptions.BinderException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+import static org.apache.felix.ipojo.Factory.INSTANCE_NAME_PROPERTY;
+
+@Component(name = "fr.liglab.adele.icasa.zigbee.dongle.factory.ZigbeeDongleFactory")
+public class ZigbeeDongleFactory {
+
+
+}
