a5a7e81a22e7015d90aea5204d4dc57ba1bea35e
aygalinc <colin.aygalinc@gmail.com>
2014-06-05 16:20:55 +0200

    Optimize import

--- platform/physical-devices/zigbee-fushia/zigbee-dongle-factory/src/main/java/fr/liglab/adele/icasa/zigbee/dongle/factory/ZigbeeDongle.java
+++ platform/physical-devices/zigbee-fushia/zigbee-dongle-factory/src/main/java/fr/liglab/adele/icasa/zigbee/dongle/factory/ZigbeeDongle.java
@@ -1,3 +1,18 @@
+/**
+ *
+ *   Copyright 2011-2013 Universite Joseph Fourier, LIG, ADELE Research
+ *   Group Licensed under a specific end user license agreement;
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://adeleresearchgroup.github.com/iCasa/snapshot/license.html
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package fr.liglab.adele.icasa.zigbee.dongle.factory;
 
 import org.apache.felix.ipojo.annotations.*;
@@ -7,7 +22,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.usb4java.*;
 import org.wisdom.akka.AkkaSystemService;
-import org.wisdom.akka.impl.AkkaScheduler;
 
 import java.util.concurrent.Callable;
 
