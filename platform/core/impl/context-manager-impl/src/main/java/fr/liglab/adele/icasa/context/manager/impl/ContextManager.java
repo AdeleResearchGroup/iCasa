fc37874c28619f68331b6be74d11412f3e33a6b9
GerbertGaillard-Eva <eva.gerbert.gaillard@gmail.com>
2017-01-24 11:08:43 +0100

    Update resolution

--- platform/core/impl/context-manager-impl/src/main/java/fr/liglab/adele/icasa/context/manager/impl/ContextManager.java
+++ platform/core/impl/context-manager-impl/src/main/java/fr/liglab/adele/icasa/context/manager/impl/ContextManager.java
@@ -1,33 +1,84 @@
 /**
- * TEMP
- * Classe principale du gestionnaire de contexte
+ *
+ *   Copyright 2011-2013 Universite Joseph Fourier, LIG, ADELE Research
+ *   Group Licensed under a specific end user license agreement;
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://adeleresearchgroup.github.com/iCasa/snapshot/license.html
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
+
 package fr.liglab.adele.icasa.context.manager.impl;
 import fr.liglab.adele.icasa.context.manager.api.ContextGoal;
 import fr.liglab.adele.icasa.context.manager.api.ContextGoalRegistration;
-import org.apache.felix.ipojo.annotations.Component;
-import org.apache.felix.ipojo.annotations.Instantiate;
-import org.apache.felix.ipojo.annotations.Provides;
+import org.apache.felix.ipojo.annotations.*;
 
-@Component
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * TEMP
+ * Classe principale du gestionnaire de contexte
+ */
+@Component(publicFactory = false)
 @Provides
 @Instantiate
-public class ContextManager implements ContextGoalRegistration {
+public class ContextManager implements ContextGoalRegistration, Runnable {
 
+    /*Runnable parameters*/
+    private static Thread thread = null;
+    private static boolean end = true;
+    private static final int DELAY = 10 * 1000; //milliseconds
 
+    private Map<String, ContextGoal> contextGoalMap = new HashMap<>();
 
-    @Override
-    public boolean registerContextManagerGoals(String appId, ContextGoal contextGoal) {
-        return false;
+    @Validate
+    public void start(){
+        thread = new Thread(this);
+        end = false;
+        thread.start();
+    }
+
+    @Invalidate
+    public void stop(){
+        end = true;
     }
 
     @Override
-    public ContextGoal getRegisteredContextManagerGoals(String appId) {
-        return null;
+    public boolean registerContextGoals(String appId, ContextGoal contextGoal) {
+        contextGoalMap.put(appId,contextGoal);
+        thread.interrupt(); //refresh
+        return true;
     }
 
     @Override
-    public boolean unregisterContextManagerGoals(String appId) {
-        return false;
+    public ContextGoal getRegisteredContextGoals(String appId) {
+        return contextGoalMap.get(appId);
+    }
+
+    @Override
+    public boolean unregisterContextGoals(String appId) {
+        contextGoalMap.remove(appId);
+        return true;
+    }
+
+
+    @Override
+    public void run() {
+        while (!end) {
+            try {
+                Thread.sleep(DELAY);
+            } catch (InterruptedException ie) {
+                /* will recheck quit */
+            } finally {
+                /*TODO do something*/
+            }
+        }
     }
 }
---
+++ platform/core/impl/context-manager-impl/src/main/java/fr/liglab/adele/icasa/context/manager/impl/ContextResolutionMachine.java
@@ -0,0 +1,9 @@
+package fr.liglab.adele.icasa.context.manager.impl;
+
+/**
+ * TEMP
+ * Classe qui détermine la meilleure configuration avec les buts et l'environnement actuel
+ */
+public class ContextResolutionMachine {
+
+}
