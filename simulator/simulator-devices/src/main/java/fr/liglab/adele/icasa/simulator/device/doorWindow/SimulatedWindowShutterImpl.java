524039236dc1548186375b5d9b4eeac75b7b92de
Eva <eva.gerbert.gaillard@gmail.com>
2017-09-13 17:22:11 +0200

    Simulator repackaging
    (shutter in windowDoor instead of temperature)

--- simulator/simulator-devices/src/main/java/fr/liglab/adele/icasa/simulator/device/doorWindow/SimulatedWindowShutterImpl.java
+++ simulator/simulator-devices/src/main/java/fr/liglab/adele/icasa/simulator/device/doorWindow/SimulatedWindowShutterImpl.java
@@ -13,7 +13,7 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
-package fr.liglab.adele.icasa.simulator.device.temperature.impl;
+package fr.liglab.adele.icasa.simulator.device.doorWindow;
 
 import fr.liglab.adele.cream.annotations.entity.ContextEntity;
 import fr.liglab.adele.cream.annotations.functional.extension.FunctionalExtension;
